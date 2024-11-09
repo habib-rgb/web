a5e31b079a9dc362b9bb1baaaeca2c0303a98ee2
habib-rgb <149590570+habib-rgb@users.noreply.github.com>
2024-11-09 21:57:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function sendApology() {
+    const message = document.getElementById('apology-message');
+    message.textContent = "Terima kasih cantik abib, cantik abib lah wanita nan abib sayangi!";
+}
